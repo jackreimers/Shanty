432fea640e09f3b6cc87aa5d854c6e756a12c131
Jack Reimers <jack.reimers.00@gmail.com>
2024-01-13 19:56:30 +1000

    Add basic header responsiveness

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,15 +3,15 @@
 	import Button from '$lib/components/button.svelte';
 </script>
 
-<header class="border-b border-gray-700 py-5">
-	<div class="container mx-auto flex">
+<header class="border-b border-gray-700 py-3 md:py-5">
+	<div class="container mx-auto px-3 sm:flex">
 		<div class="flex flex-1 items-center">
 			<div>
-				<h1 class="mb-1 text-4xl font-bold">Bunbury Sea Shanty Festival</h1>
+				<h1 class="text-2xl font-bold md:mb-1 md:text-4xl">Bunbury Sea Shanty Festival</h1>
 				<p>Tagline about the event</p>
 			</div>
 		</div>
-		<div class="flex items-center">
+		<div class="hidden items-center sm:flex">
 			<Button classes="border border-gray-700 px-3.5 py-2.5 font-semibold leading-none">
 				Tickets
 				<Icon icon="open_in_new" classes="text-base" />
