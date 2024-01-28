778243d6a37ed53ca709b590cb3009aef05e0443
Jack Reimers <jack.reimers.00@gmail.com>
2024-01-28 21:54:21 +1000

    Fix footer responsiveness

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -4,15 +4,17 @@
 
 <footer class="bg-slate-600 py-8 text-white">
 	<Container>
-		<div class="flex justify-center text-center leading-none">
-			<p class="mr-2 border-r-2 border-slate-500 pr-2 font-medium text-slate-400">
-				Copyright © 2024
-			</p>
-			<p class="font-medium text-slate-300">
+		<div class="text-center sm:flex sm:flex-row-reverse sm:justify-center sm:leading-none">
+			<p class="mb-1 font-medium text-slate-300 sm:mb-0">
 				Site built by
 				<a href="https://jackreimers.dev" class=" font-semibold text-white hover:underline"
 					>Jack Reimers</a
 				>
+			</p>
+			<p
+				class=" font-medium text-slate-400 sm:mr-2 sm:border-r-2 sm:border-slate-500 sm:pr-2"
+			>
+				Copyright © 2024
 			</p>
 		</div>
 	</Container>
