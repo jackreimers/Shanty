1bab0ebbfa60fd1a2cc9ca7f6c9de950ab4cda4b
Jack Reimers <jack.reimers.00@gmail.com>
2024-02-29 13:13:01 +1000

    Add programs page

---
+++ src/routes/programs/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Hero from '$lib/components/layout/hero.svelte';
+	import Container from '$lib/components/layout/container.svelte';
+</script>
+
+<svelte:head>
+	<title>Bunbury Sea Shanty Festival | Program</title>
+</svelte:head>
+
+<Hero>
+	<div slot="content">
+		<p class="mb-4 text-2xl font-bold lg:text-4xl">Programs</p>
+		<p class="max-w-prose">
+			Fermentum dui faucibus in ornare quam viverra orci. Non diam phasellus vestibulum lorem
+			sed risus ultricies tristique nulla.
+		</p>
+	</div>
+</Hero>
+
+<Container>
+	<p class="text-2xl font-semibold">Coming soon!</p>
+</Container>
