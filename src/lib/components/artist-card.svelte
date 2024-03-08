a1c370a9313b7c339b4df08d66f4c574cd8a409f
Jack Reimers <jack.reimers.00@gmail.com>
2024-03-08 23:03:01 +1000

    Remove unused elements from artist cards

--- src/lib/components/artist-card.svelte
+++ src/lib/components/artist-card.svelte
@@ -8,9 +8,13 @@
 </script>
 
 <div class="rounded bg-gray-100 p-3.5">
+	<!--
 	<div class="mb-4 rounded bg-white py-20 text-center shadow-inner">
 		<Icon icon="image" classes="text-6xl text-gray-400" />
 	</div>
+	-->
 	<p class="mb-1 font-semibold">{name}</p>
+	<!--
 	<p>{description}</p>
+	-->
 </div>
