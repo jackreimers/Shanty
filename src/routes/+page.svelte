493f8599ff8ac3ee042b7a670c67787496689ee5
Jack Reimers <jack.reimers.00@gmail.com>
2024-03-25 00:14:19 +1000

    Add sponsors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 			class="block min-h-[150px] flex-1 rounded bg-[url('/images/bunbury-waves.jpg')] bg-cover bg-center"
 		/>
 		<div class="flex-[2] rounded bg-gray-100 p-4 md:p-6">
-			<h2 class="header mb-2 text-2xl font-semibold">About The Festival</h2>
+			<h2 class="header mb-2 text-2xl font-semibold sm:text-3xl">About The Festival</h2>
 			<p>
 				Ahoy, mates! Welcome to the inaugural Bunbury Sea Shanty Festival, where the waves
 				meet the melodies in Western Australia's charming coastal city Bunbury. Join us for
@@ -29,5 +29,12 @@
 				lifetime.
 			</p>
 		</div>
+	</div>
+	<h2 class="header mb-2 mt-8 text-2xl font-semibold sm:text-3xl">Our Sponsors</h2>
+	<div class="grid grid-cols-2 items-center gap-6 rounded bg-gray-100 p-4 md:p-6 lg:grid-cols-4">
+		<img src="/images/cob.png" alt="City of Bunbury logo" />
+		<img src="/images/staidan.png" alt="St Aidans Winery Logo" />
+		<img src="/images/svitzer.png" alt="Svitzer Logo" />
+		<img src="/images/aqwest.png" alt="Aqwest Logo" />
 	</div>
 </Container>
