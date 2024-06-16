d5666b289b3065283382c4bf4e3c658115c9e907
Jack Reimers <jack.reimers.00@gmail.com>
2024-06-16 10:53:48 +1000

    Add volunteering page details

--- src/routes/volunteer/+page.svelte
+++ src/routes/volunteer/+page.svelte
@@ -14,5 +14,29 @@
 </Hero>
 
 <Container>
-	<p class="text-2xl font-semibold">Coming soon!</p>
+	<p class="mb-2">
+		We're looking for enthusiastic volunteers to join us for the upcoming Bunbury Sea Shanty
+		Festival! As a volunteer, you'll have the opportunity to be part of an amazing team and
+		contribute to the success of the festival.
+	</p>
+
+	<p class="mb-2">Roles include (but are not limited to):</p>
+	<ul class="mb-2 ml-6 list-decimal">
+		<li>Front of House</li>
+		<li>MC</li>
+		<li>Tech Support (experience desired)</li>
+		<li>Retail / Merchandise.</li>
+	</ul>
+
+	<p class="mb-2">
+		Each volunteer will receive a free weekend pass (after 6 hours of volunteering) and a
+		t-shirt.
+	</p>
+
+	<p class="mb-2">
+		If you're interested in being part of this exciting event, please email us at
+		contact@bunburyshantyfest.au
+	</p>
+
+	<p>Note: Please also refer to the FAQ section of the website, for more information</p>
 </Container>
